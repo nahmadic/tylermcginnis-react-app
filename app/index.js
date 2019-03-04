3c08bcff3e7cad6d32c827c5477335854e4ed0ce
Ryan Browne <brownetown7@gmail.com>
2019-03-04 20:25:23 +0000

    Installs and uses PropTypes

--- app/index.js
+++ app/index.js
@@ -1,18 +1,35 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-require('./index.css');
+var PropTypes = require('prop-types');
+// require('./index.css');
 
-class App extends React.Component {
+class Badge extends React.Component {
   render() {
     return (
       <div>
-        Hello World!
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>Username: {this.props.username}</h3>
       </div>
     )
   }
 }
 
+Badge.PropTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+}
+
 ReactDOM.render(
-  <App />,
+  <Badge 
+    name='Tyler McGinnis'
+    username='tylermcginnis'
+    img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+  />,
   document.getElementById('app')
 );
