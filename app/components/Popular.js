e17c927cad672d6bcddbde1394a876c87f8a9119
Ryan Browne <brownetown7@gmail.com>
2019-03-07 21:56:29 +0000

    Adds API file and methods

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage (props) {
   var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -30,22 +31,35 @@ class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount () {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(lang) {
     this.setState(function () {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
     });
+
+    api.fetchPopularRepos(lang).then(function (repos) {
+      this.setState(function () {
+        return {
+          repos: repos
+        }
+      })
+    }.bind(this));
   }
 
   render() {
-
     return (
       <div>
         <SelectLanguage 
