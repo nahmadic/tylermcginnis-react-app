d4c883ca6841ee328ea797cd542da7a293ea0063
Ryan Browne <brownetown7@gmail.com>
2019-03-09 23:54:07 +0000

    Adds battle state management

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,9 +1,35 @@
 var React = require('react');
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+
+    this.handleSubmit = this.handeSubmit.bind(this);
+  }
+
+  handeSubmit(id, username) {
+    this.setState(function () {
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      return newState;
+    });
+  }
+
   render() {
     return (
-      <h1>Battle goes here</h1>
+      <div>
+        <div className='row'>
+          
+        </div>
+      </div>
     )
   }
 }
