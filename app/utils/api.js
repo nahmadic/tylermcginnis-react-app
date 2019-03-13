78be8bb60e13c4028cb6073f9182053133a4f6b7
Ryan Browne <brownetown7@gmail.com>
2019-03-13 20:27:27 +0000

    Adds example code for conncting to Github API

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,10 @@
 var axios = require('axios');
 
+// This allows you to connect to the Github API consistently
+// var id = "YOUR_CLIENT_ID";
+// var sec = "YOUR_SECRET_ID";
+// var params = "?client_id=" + id + "&client_secret=" + sec;
+
 module.exports = {
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
